0d02b3292060b401ade7309e0d4321472456e09a
Ihor Didyk <ihordidyk95@gmail.com>
2024-02-24 07:42:42 +0200

    [draft] update spring boot to 3.2.3, commented jackson hints from runtime import

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.helpers.DataGenerator;
-import com.example.demo.hints.JacksonRuntimeHints;
 import com.example.demo.hints.LiquibaseRuntimeHints;
-import com.example.demo.model.Company;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints({LiquibaseRuntimeHints.class, JacksonRuntimeHints.class})
+@ImportRuntimeHints({LiquibaseRuntimeHints.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
